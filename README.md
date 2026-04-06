4f70f421bc4fe04b4c1b755324b3aeb6e7be3991
ANIRBAN ADHIKARY <54547722+MN-ANIRBAN@users.noreply.github.com>
2026-04-07 01:38:57 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# MNAgent
-A professional dynamic dependency guard that auto-manages Python packages.
+# MN-AGENT v2.4.0
+Dynamic Dependency Guard for Python.
+
+## Installation
+`pip install git+https://https://github.com/MN-ANIRBAN/MNAgent.git`
+
+## Usage
+Just add this to the top of your `main.py`:
+```python
+import mnagent2
+# Your code follows...
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MN_Agent2.4",
+    version="2.4",
+    packages=find_packages(),
+    author="ANIRBAN ADHIKARY",
+    description="A dynamic dependency guard that auto-manages python packages.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://https://github.com/MN-ANIRBAN/MNAgent", # GitHub link
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[], 
+)
